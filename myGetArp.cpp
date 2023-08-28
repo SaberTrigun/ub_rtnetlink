98ef2bec4db23cb393c6ed42908191735712acd1
trigun <00700590@tutanota.com>
2023-08-29 00:12:34 +0200

    Need parse

--- myGetArp.cpp
+++ myGetArp.cpp
@@ -6,20 +6,21 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <linux/rtnetlink.h>
+#include <net/netlink.h>
 using namespace std;
 
 #define NUD_VALIDE (NUD_PERMANENT|NUD_NOARP|NUD_REACHABLE|NUD_PROBE|NUD_STALE|NUD_DELAY)
 #define GRP_SUBSCRIBE (RTMGRP_IPV6_IFADDR | RTMGRP_NEIGH | RTMGRP_IPV4_IFADDR)
 
-void parseRtAttr(struct rtattr *tb[], int max, struct rtattr *pRta, int len)
+void parseRtAttr(struct nlattr *tb[], int max, struct nlattr *pRta, int len)
 {
-    memset(tb, 0, sizeof(struct rtattr*) * (max + 1));
+    memset(tb, 0, sizeof(struct nlattr*) * (max + 1));
 
     while(RTA_OK(pRta, len))
     {
-        if (pRta->rta_type <= max)
+        if (pRta->nla_type <= max)
         {
-            tb[pRta->rta_type] = pRta;
+            tb[pRta->nla_type] = pRta;
         }
         pRta = RTA_NEXT(pRta, len);
     }
@@ -28,93 +29,90 @@ void parseRtAttr(struct rtattr *tb[], int max, struct rtattr *pRta, int len)
 
 int main(){
 
-    struct nslmsghdr    *pHeaderAnswer = nullptr;
+    uint8_t buf[256 * 1024];
+
+    struct nlmsghdr*    pHeaderAnswer;
     struct sockaddr_nl  local;
-    struct iovec        iov;
+    struct iovec        iov {.iov_base = buf, .iov_len = sizeof(buf)};
     struct msghdr       msg;
+    struct ndmsg*       pNdmsg;
+    struct nlattr*      tb[NDA_MAX + 1];
 
 
     struct{
         nlmsghdr header;
         ndmsg    neighMsg;
-        char     attrBuf[8192];
     }req;
 
+
     memset(&local, 0, sizeof(local));
-    local.nl_family     = AF_NETLINK;
-    local.nl_pid        = 0;
-    //local.nl_groups     = RTMGRP_IPV6_IFADDR | RTMGRP_NEIGH | RTMGRP_IPV4_IFADDR;
+    local.nl_family = AF_NETLINK;
+    local.nl_pid = 0;
+    local.nl_groups = 0;
 
     memset(&req, 0, sizeof(req));
-    req.header.nlmsg_len    = NLMSG_LENGTH(sizeof(struct ndmsg));
-    req.header.nlmsg_flags  = NLM_F_REQUEST | NLM_F_DUMP;
-    req.header.nlmsg_type   = RTMGRP_IPV6_IFADDR | RTMGRP_NEIGH | RTMGRP_IPV4_IFADDR;
-    req.neighMsg.ndm_state  = NUD_PERMANENT|NUD_NOARP|NUD_REACHABLE|NUD_PROBE|NUD_STALE|NUD_DELAY;
+    req.header.nlmsg_len = NLMSG_LENGTH(sizeof(req.neighMsg));
+    req.header.nlmsg_type = RTM_GETNEIGH;
+    req.header.nlmsg_flags = NLM_F_REQUEST | NLM_F_DUMP;
     req.neighMsg.ndm_family = AF_INET;
-    req.header.nlmsg_pid    = 0;
-    req.header.nlmsg_seq    = 1;
-
-    memset(&msg, 0, sizeof(msg));
-    iov.iov_base = (void*)&req.header;
-    iov.iov_len  = req.header.nlmsg_len;
-
-    msg.msg_iov     = &iov;
-    msg.msg_iovlen  = 1;
 
 
     int sock = socket(AF_NETLINK, SOCK_RAW, NETLINK_ROUTE);
     if(sock >= 0)
     {
-        cout << "Sock: " << sock << " OK!!!" << endl;
-        if(bind(sock, (struct sockaddr*) &local, sizeof(local)) >= 0)
+        cout<<"Sock: " << sock << " OK!!!" << endl;
+        if(bind(sock, (struct sockaddr*)&local, sizeof(local)) >= 0)
         {
             cout << "Bind: OK!!!" << endl;
-            ssize_t sendRes = sendmsg(sock, &msg, 0);
-            if(sendRes>= 0)
+            int resSend = send(sock, &req, req.header.nlmsg_len, 0);
+            if(resSend > 0)
             {
-                cout<< "SendRes: " << sendRes<< " OK!!!" << endl;
-                struct iovec    ioAnswer;
-                struct msghdr   msgHdr;
-                memset(&ioAnswer, 0, sizeof(iovecAnswer));
-                memset(&msgHdr, 0, sizeof(msgHdr));
+                cout << "Send: " << resSend << " OK" << endl;
 
-                ioAnswer.iov_base = msgHdr;
-                ioAnswer.iov_len  = IFLIST_REPLY_BUFFER;
-                msgHdr.msg_iov = &ioAnswer;
-                msgHdr.msg_iovlen = 1;
-                msgHdr.msg_name = &kernell
-                msgHdr.msg_namelen = sizeof(kernel);
-                msgHdr.
+                memset(&msg, 0, sizeof(msg));
+                msg.msg_iov = &iov;
+                msg.msg_iovlen =1;
+                msg.msg_controllen = 0;
+                msg.msg_control = NULL;
+                msg.msg_flags =0;
 
-                ssize_t rcvRes =recvmsg(sock, &msg, MSG_DONTWAIT);
-                if(rcvRes >= 0)
+                int len = recvmsg(sock, &msg, 0);
+                if(len >0)
                 {
-                    cout << "RcvRes: " << rcvRes << " OK!!!" << endl;
+                    cout << "ResRcv: " << len<< " OK!!!" << endl;
 
-                    //for()
+                    memset(tb, 0, sizeof(tb));
+                    pHeaderAnswer = (struct nlmsghdr*)buf;
+                    while(NLMSG_OK(pHeaderAnswer, len))
                     {
-                        
+                        pNdmsg = (struct ndmsg*)NLMSG_DATA(pHeaderAnswer);
+                        parseRtAttr(tb, NDA_MAX, NDA_HDRLEN(pNdmsg), pHeaderAnswer->nlmsg_len)
+
                     }
                 }
-                else
-                {
-                    cout << "RcvRes: " << rcvRes << " BAD!!!" << endl;
-                }
-            }
-            else
-            {
-                cout << "SendRes: " << sendRes << " BAD!!!" << endl;
             }
         }
-        else
-        {
-            cout << "Bind: BAD BIND!!" << endl;
-        }
     }
-    else
-    {
-        cout << "Sock: BAD SOCK!!!" << endl;
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
